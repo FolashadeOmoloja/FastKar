22883642425da5361033e57605942af138c1bb55
FolashadeOmoloja <omolojashade@gmail.com>
2023-08-28 21:57:40 +0100

    changes

--- components/Process.tsx
+++ components/Process.tsx
@@ -6,36 +6,36 @@ import {BsCalendarHeartFill} from 'react-icons/bs'
 
 const Process = () => {
   return (
-   <section id="#how-it-works" className="flex flex-col pt-10 items-center p-[3rem] max-xsm:p-[2rem] max-xsm:pb-0 pb-0 ">
-       <h3 className="mt-5 me-5 text-[#5e5f5f] text-sm">HOW IT WORKS</h3>
-        <h2 className="text-lg">Your Path to Seamless and Effortless Travel Solutions</h2>
-        <section className="flex">
+   <section id="how-it-works" className="flex flex-col pt-[130px] items-center p-[3rem] max-xsm:pb-0 pb-0 ">
+       <h3 className="mb-1 text-[#494949] text-sm">HOW IT WORKS</h3>
+        <h2 className="text-[30px] text-center font-bold mb-12">Your Path to Seamless Travel Solutions</h2>
+        <section className="flex max-sm:flex-col max-sm:gap-3">
             <div className="flex flex-col text-center items-center gap-3">
                   <div className="bg-[#f2f2f2] process-cards">
                        <span className="text-[#2387FE] text-[30px]"><HiLocationMarker/></span>
                   </div>
-                  <span>Set a destination</span>
-                  <span>Enter your destination to book your ride conveniently</span>
+                  <span className="font-semibold">Set a destination</span>
+                  <span className="text-sm ">Enter your destination to book your ride conveniently</span>
             </div>
             <div >
-              <Image width={100} height={50} src='/right-arrow.webp' alt="arrow" className="max-xsm:hidden"/>
+              <Image width={100} height={50} src='/right-arrow.webp' alt="arrow" className="max-md:hidden"/>
             </div>
             <div className="flex flex-col text-center items-center gap-3">
                   <div className="bg-bluegradient process-cards">
                        <span className="text-white text-[30px]"><BsFillCarFrontFill/></span>
                   </div>
-                  <span>Select a vehicle</span>
-                  <span>Pick your ride, choose your preferred vehicle with ease</span>
+                  <span className="font-semibold">Select a vehicle</span>
+                  <span className="text-sm ">Pick your ride, choose your preferred vehicle with ease</span>
             </div>
             <div>
-              <Image width={100} height={50} src='/right-arrow.webp' alt="arrow" className="max-xsm:hidden"/>
+              <Image width={100} height={50} src='/right-arrow.webp' alt="arrow" className="max-md:hidden"/>
             </div>
             <div className="flex flex-col text-center items-center gap-3 ">
                   <div className="bg-[#f2f2f2] process-cards">
                        <span className="text-[#2387FE] text-[30px]"><BsCalendarHeartFill/></span>
                   </div>
-                  <span>Book your Ride!</span>
-                  <span>Book your ride, seize the journey ahead effortlessly.</span>
+                  <span className="font-semibold">Book your Ride!</span>
+                  <span className="text-sm ">Book your ride, seize the journey ahead effortlessly.</span>
             </div>
         </section>
    </section>
