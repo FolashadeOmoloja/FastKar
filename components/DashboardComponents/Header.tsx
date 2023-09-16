7c42b67f24bc32cd3479b504996693dbb936ab16
FolashadeOmoloja <omolojashade@gmail.com>
2023-09-16 21:42:01 +0100

    changes

--- components/DashboardComponents/Header.tsx
+++ components/DashboardComponents/Header.tsx
@@ -1,11 +1,27 @@
 import Image from "next/image"
+import {BiSolidChevronDown, BiSolidBellRing, BiSolidBell} from 'react-icons/bi'
+import {BsFillBellFill} from 'react-icons/bs'
+
 
 const Header = () => {
   return (
-   <header>
+   <header className="flex">
        <section>
           <Image 
           src={"/logo.png"} alt={"logo"} width={125} height={125} className="max-xsm:w-[100px] max-xsm:h-[100px]"/>
+       </section>
+       <section>
+          <p>Welcome back,Shades!</p>
+       </section>
+       <section>
+         <div>
+            <img src="" alt="" />
+         </div>
+         <p>Shade Omoloja</p>
+         <BiSolidChevronDown/>
+       </section>
+       <section>
+         <BsFillBellFill/>
        </section>
    </header>
   )
