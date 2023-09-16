225f3a05299442b5c2600505ba31d77a4e5604e4
FolashadeOmoloja <omolojashade@gmail.com>
2023-09-16 22:10:14 +0100

    changes

--- components/DashboardComponents/Header.tsx
+++ components/DashboardComponents/Header.tsx
@@ -5,22 +5,22 @@ import {BsFillBellFill} from 'react-icons/bs'
 
 const Header = () => {
   return (
-   <header className="flex">
+   <header className="bg-color pr-[40px] max-xsm:pr-[10px] flex justify-between items-center h-[100px] border-gray-400 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
        <section>
           <Image 
-          src={"/logo.png"} alt={"logo"} width={125} height={125} className="max-xsm:w-[100px] max-xsm:h-[100px]"/>
+          src={"/logo.png"} alt={"logo"} width={100} height={100} className="pl-[10px]"/>
        </section>
-       <section>
-          <p>Welcome back,Shades!</p>
+       <section className="max-sm:hidden">
+          <p className="font-semibold text-lg">Welcome back,Shades!</p>
        </section>
-       <section>
+       <section className="flex items-center gap-2 text-sm">
          <div>
-            <img src="" alt="" />
+            <img src="/user3.png" alt=""  className="w-[50px] h-[50px] rounded-full"/>
          </div>
-         <p>Shade Omoloja</p>
+         <p >Shade Omoloja</p>
          <BiSolidChevronDown/>
        </section>
-       <section>
+       <section className="max-xsm:hidden">
          <BsFillBellFill/>
        </section>
    </header>
