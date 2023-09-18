b1392d143fc0fab927392930ccdcd0ee5b7c0f9b
FolashadeOmoloja <omolojashade@gmail.com>
2023-09-18 12:02:47 +0100

    changes

--- components/DashboardComponents/SideNav/SideNav.tsx
+++ components/DashboardComponents/SideNav/SideNav.tsx
@@ -2,7 +2,8 @@ import { NavItem } from "@/constants/constants"
 
 export const SideNav = () => {
   return (
-  <nav className="sticky top-[100px] left-0 bottom-0 h-[100vh] overflow-y-scroll bg-color w-[270px] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+  <nav className="fixed top-[100px] left-0 bottom-0 h-[100vh]  bg-color w-[270px]  flex flex-col items-center py-7 border-r-2 border-[rgba(0,_0,_0,_0.24)]">
+    {/* <span className="font-semibold text-lg">Menu</span> */}
       <ul>
 {
   NavItem.map((item,index) =>{
