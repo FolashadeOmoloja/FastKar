6efedb5ff65cd2918e3cb4b88434a858e8ade482
FolashadeOmoloja <omolojashade@gmail.com>
2023-09-29 16:58:14 +0100

    bookings

--- components/DashboardComponents/Bookings/Map.tsx
+++ components/DashboardComponents/Bookings/Map.tsx
@@ -1,5 +1,8 @@
 'use client'
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import L from 'leaflet'
+
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
@@ -8,10 +11,23 @@ const Map = () => {
            <MapContainer 
               center={[51.505, -0.09]} 
               zoom={13} 
-              className='w-full h-[70vh] rounded-lg '
+              className='w-full md:h-full max-md:h-[70vh] rounded-lg '
            >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[51.505, -0.09]} />
+      <Marker icon={
+                    new L.Icon({
+                        iconUrl: '/locationmarker.png',
+                        iconRetinaUrl: '/locationmarker.png',
+                        iconSize: [25, 30],
+                        iconAnchor: [12.5, 41],
+                        popupAnchor: [0, -41],
+                        shadowSize: [41, 41],
+                    })
+                } position={[51.505, -0.09]}>
+                     <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
     </MapContainer>
     </section>
   )
