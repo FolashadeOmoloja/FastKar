7d7946ab855e1aeda90c1b1dce17cdbecd6ff136
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-05 19:20:29 +0100

    trip details

--- components/DashboardComponents/Bookingss/TripDetails/TripDetails.tsx
+++ components/DashboardComponents/Bookingss/TripDetails/TripDetails.tsx
@@ -1,4 +1,4 @@
-
+import { useForm } from 'react-hook-form';
 
 
 interface Iprop {
@@ -9,7 +9,16 @@ const TripDetails:React.FC<Iprop> = ({tripDetailObject}) => {
 
     console.log(tripDetailObject)
   return (
-    <div>TripDetails</div>
+   <section className='shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+      <div className='bg-[#f9f9f9] '>
+       <span className='font-semibold text-[20px]'>Booking Details</span>
+      </div>
+      <form action="">
+          <div>
+
+          </div>
+      </form>
+   </section>
   )
 }
 
