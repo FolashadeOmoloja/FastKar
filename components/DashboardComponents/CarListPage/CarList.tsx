9f9b38c5c4eaad5bc17fe3ec39053b76e1ffba8f
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-07 19:25:32 +0100

    riders

--- components/DashboardComponents/CarListPage/CarList.tsx
+++ components/DashboardComponents/CarListPage/CarList.tsx
@@ -2,20 +2,21 @@ import { vehicles } from "@/constants"
 import {PiMotorcycleFill} from 'react-icons/pi'
 import {BsFillCarFrontFill} from 'react-icons/bs'
 import {MdOutlineAirlineSeatReclineNormal, MdLuggage , MdNoLuggage} from 'react-icons/md'
-import Image from 'next/image'
+
 
 const CarListPage = () => {
   return (
     <section>
       <div className="flex justify-between mb-7 font-semibold">
-      <h3 className='text-[20px] '>Car Catalog</h3>
+      <h3 className='text-[20px] '>FastKar Catalog</h3>
       <p className='text-sm'>Explore Our Cars</p>
       </div>
+      {/* shadow-[0_3px_10px_rgb(0,0,0,0.2)] */}
       <section className="flex gap-7 flex-wrap">
            {
                vehicles.map((veh, index)=>{
                 return (
-                <div className="max-w-[300px] flex flex-col rounded-[1rem] mx-auto sm:mx-0 bg-white h-[360px] shadow-[0_3px_10px_rgb(0,0,0,0.2)] max-sm:max-w-[270px] card-details cursor-pointer">
+                <div className="max-w-[300px] flex flex-col rounded-[1rem] mx-auto sm:mx-0 bg-white h-[360px]  max-sm:max-w-[270px] card-details cursor-pointer" key={index}>
                 <p className="font-bold mb-6 px-10 pt-6 max-xsm:pt-3">{veh.type}</p>
                 <div className="px-10">
                   <img src={veh.image} alt={"img"}  className="mb-5 h-[200px] w-[340px] "/>
@@ -36,17 +37,9 @@ const CarListPage = () => {
                           </div>
                       </div>
                       <div className="book-button opacity-0 card-show-details">
-                          <span>Show Details</span>
+                          <span>View More</span>
                       </div>
                  </div>
-               {/* <div className="flex justify-between px-10">
-               <div className="flex">{stars}</div>
-               <span className="text-gray-700 text-sm ">{veh.reviews}</span>
-               </div>
-               <p className="mt-3 px-10">{veh.description}</p> */}
-               {/* <button className="w-full h-[65px] rounded-b-[1rem] bg-bluegradient mt-3 text-white font-semibold bg-200% hover:bg-right-center transition-[background] duration-[400ms] ease-in-out " >
-                   Show Details
-               </button> */}
               </div>
                 )
                })
