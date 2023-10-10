d73df180a85be12e12b3853c27f6d81aae8ac8f8
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-10 22:03:57 +0100

    riders

--- components/DashboardComponents/SideNav/SideNav.tsx
+++ components/DashboardComponents/SideNav/SideNav.tsx
@@ -2,9 +2,15 @@
 import Link from 'next/link';
 import { NavItem } from '@/constants/constants';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export const SideNav = () => {
   const [activeItem, setActiveItem] = useState(0);
+  const router = useRouter();
+
+  const handleClick = () => {
+   ;
+  };
 
   const handleItemClick = (index: number) => {
     setActiveItem(index);
@@ -14,7 +20,7 @@ export const SideNav = () => {
     <nav className="fixed top-[100px] left-0 bottom-0  bg-color w-[270px] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] flex flex-col items-center py-8 border-r-2 border-[rgba(0,_0,_0,_0.24)] max-slg:hidden">
       <ul>
         {NavItem.map((item, index) => (
-          <Link href={item.href} key={index}>
+          <div onClick={()=> router.push(item.href)} key={index} className='cursor-pointer'>
             <li
               onClick={() => handleItemClick(index)} // 
               className={`nav-hover flex justify-between w-[210px] relative text-gray-700 mb-[20px] transition duration-300 rounded-lg bg-[#E3F0F9] pl-[10px] py-2 ${
@@ -29,7 +35,7 @@ export const SideNav = () => {
               </div>
               <div className="w-[20px] h-[50px] absolute top-0 right-0 bg-[#AED6EF] rounded-l-full"></div>
             </li>
-          </Link>
+          </div>
         ))}
       </ul>
     </nav>
