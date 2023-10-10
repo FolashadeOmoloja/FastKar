ca1e0560ab1a96538b1179dbb8a4b2b9d62ce95a
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-10 16:15:14 +0100

    riders

--- components/DashboardComponents/Riders/Riders.tsx
+++ components/DashboardComponents/Riders/Riders.tsx
@@ -1,4 +1,4 @@
-
+import { DriverData } from "@/constants"
 
 const Riders = () => {
   return (
@@ -8,8 +8,26 @@ const Riders = () => {
          <h3 className='text-[20px] '>FastKar Drivers</h3>
          <p className='text-sm max-sm:hidden'>Know who's driving you</p>
       </div>
-      <section>
-        
+      <section className="grid grid-cols-2 gap-3 max-sm:grid-cols-1">
+             {
+                DriverData.map((veh,index)=>{
+                    return(
+                        <section key={index} className="flex p-2 w-[450px] max-lg:flex-col max-xlg:w-[340px] bg-white max-md:w-[290px] max-xsm:w-[280px]">
+                             <div className="basis-1/2 h-full bg-gray-200 flex items-center justify-center">
+                                <img src={veh.image} alt="driver" className="w-full h-full" />
+                             </div>   
+                             <div className="flex flex-col gap-2 basis-1/2">
+                                 <p className="p-3 bg-gray-200">FastKar</p>
+                                 <span>{veh.name}</span>
+                                 <p>{veh.description}</p>
+                                 <p>{veh.qualifications}</p>
+                                 <button className="border-b bg-transparent">View More</button>
+                            </div>  
+                        </section>
+                    )
+                })
+             }
+
       </section>
     </section>
   )
