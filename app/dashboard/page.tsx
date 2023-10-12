e4b219798ee4e1dfb042ee78e3e3cbbb756259a9
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-12 21:25:53 +0100

    riders

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Homepage from "@/components/DashboardComponents/Home/Homepage"
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+       <Homepage/>
+    </div>
   )
 }
 
