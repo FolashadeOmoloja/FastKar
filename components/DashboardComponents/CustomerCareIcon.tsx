e18e092db28b325fa955e4e3350ffb0dbc6f466c
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-12 16:05:54 +0100

    riders

--- components/DashboardComponents/CustomerCareIcon.tsx
+++ components/DashboardComponents/CustomerCareIcon.tsx
@@ -2,6 +2,8 @@
 import { useState } from 'react';
 import { TiMessages } from 'react-icons/ti';
 import { ImCross } from 'react-icons/im';
+import {BsWhatsapp} from 'react-icons/bs'
+import {MdEmail} from 'react-icons/md'
 
 const CustomerCareIcon = () => {
   const [showCross, setShowCross] = useState(false);
@@ -11,8 +13,10 @@ const CustomerCareIcon = () => {
   };
 
   return (
-    <div>
-      <div className={`text-lg text-white bg-bluegradient w-[50px] h-[50px] rounded-[50%] flex items-center justify-center bg-red-400 icon-container ${showCross ? 'rotate' : ''}` } onClick={handleClick}>
+    <div className='relative'>
+       <div className={`bg-[#4285f4] helpline absolute  customercarediv ${showCross?'top-[0]':'top-[-120px]'}`}><MdEmail/></div> 
+      <div className={`bg-[#25D366] helpline absolute customercarediv ${showCross?'top-[-0px] ':'top-[-60px] '}`}><BsWhatsapp/></div>
+      <div className={`bg-bluegradient customercarediv icon-container ${showCross ? 'rotate' : ''}` } onClick={handleClick}>
         {showCross ? <ImCross /> : <TiMessages />}
       </div>
     </div>
