f4f2a9f9e378ea66a0b75d41ef41e632b3ef96dc
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-14 16:09:33 +0100

    dashboard home

--- components/DashboardComponents/Home/Homepage.tsx
+++ components/DashboardComponents/Home/Homepage.tsx
@@ -1,8 +1,20 @@
+import Process from '@/components/Process'
 import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <section className='flex flex-col items-center'>
+        <h1 className='text-[30px] font-bold'>Welcome to your Fastkar Dashboard</h1>
+        <div>
+            <img src='/welcome.png' alt="" />
+        </div>
+
+         <section>
+             <Process/>
+         </section>
+
+
+    </section>
   )
 }
 
