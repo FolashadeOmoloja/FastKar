c3266cb1f697f9bc54b569ff2847514f0a82523f
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-14 16:21:05 +0100

    dashboard home

--- components/Process.tsx
+++ components/Process.tsx
@@ -3,10 +3,12 @@ import {HiLocationMarker} from 'react-icons/hi'
 import {BsFillCarFrontFill} from 'react-icons/bs'
 import {BsCalendarHeartFill} from 'react-icons/bs'
 
-
-const Process = () => {
+interface Iprop{
+     pt?: string
+}
+const Process:React.FC<Iprop> = ({pt='pt-[130px]'}) => {
   return (
-   <section id="how-it-works" className="flex flex-col pt-[130px] items-center px-[3rem] max-xsm:px-[32px]">
+   <section id="how-it-works" className={`flex flex-col ${pt} items-center px-[3rem] max-xsm:px-[32px]`}>
        <h3 className="mb-1 text-[#494949] text-sm">HOW IT WORKS</h3>
         <h2 className="text-[30px] text-center font-bold mb-12">Your Path to Seamless Travel Solutions</h2>
         <section className="flex max-sm:flex-col max-sm:gap-3">
