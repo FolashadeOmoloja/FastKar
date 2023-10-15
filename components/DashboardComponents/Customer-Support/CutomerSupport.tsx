89e84683ef612fa1ea0870560f7312e874be3dc1
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-15 22:46:03 +0100

    customer

--- components/DashboardComponents/Customer-Support/CutomerSupport.tsx
+++ components/DashboardComponents/Customer-Support/CutomerSupport.tsx
@@ -1,8 +1,23 @@
+import CustomerCareIcon from "../CustomerCareIcon"
+import FaqBox from "./FAQ"
 
 
 const CutomerSupport = () => {
   return (
-    <div>CutomerSupport</div>
+   <section>
+          <h3>Customer Support</h3>
+          <section className="flex max-md:flex-col">
+              <div className="basis-1/2">
+                 <img src="/customercare.webp" alt="img" />
+              </div>
+              <div className="basis-1/2">
+                 <FaqBox/>
+              </div>
+          </section>
+          <div className="fixed right-[20px] bottom-[50px] ">
+            <CustomerCareIcon/>
+        </div>
+   </section>
   )
 }
 
