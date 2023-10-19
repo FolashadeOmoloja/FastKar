6c3a5cbf44416b55f01897334351eed021466314
FolashadeOmoloja <omolojashade@gmail.com>
2023-10-19 01:53:26 +0100

    track bookings

--- components/DashboardComponents/TrackBookings/TrackBooking.tsx
+++ components/DashboardComponents/TrackBookings/TrackBooking.tsx
@@ -12,12 +12,18 @@ import {db} from '@/app/api/firebase/config'
 import { useEffect, useState } from 'react';
 import { getSession } from "next-auth/react";
 import { Session } from 'next-auth';
+import { useRouter } from 'next/navigation';
 
 const TrackBooking = () => {
     
   const [userId,setUserId] = useState<any>()
   const [bookingHistory, setBookingHistory] = useState([])
-
+  const bookingArr: { [x: string]: any; id: string; }[] = []
+  const sample=[]
+  const router = useRouter();
+  const handleClick = () => {
+      router.push('/dashboard/bookings');
+    };
    //getSession
    let session: Session | {user: any} |null;
 
@@ -29,7 +35,11 @@ const getTheSession = async () => {
 };
 
 useEffect(() => {
-  getTheSession()
+  getTheSession().then((session) => {
+    if (session) {
+      setUserId((session as { user: any }).user?.email ?? 'defaultEmail');
+    }
+  });
 }, []);
   
 
@@ -45,22 +55,42 @@ useEffect(() => {
           itemsArr.push({ ...doc.data(), id: doc.id });
         });
         itemsArr.forEach((item)=>{
-          console.log(item?.userId)
              if(session?.user?.email  == item?.userId){
-              console.log('hi')
+              bookingArr.push(item)
              }
         })
-        // console.log(itemsArr)
+        setBookingHistory(bookingArr as [])
       
       });
     }, []);
+    console.log(bookingHistory)
+    //romans 5:15
   return (
         <section>
              <h3 className="text-[30px] font-semibold text-[#2387FE]">Track Bookings</h3>
              <span className="text-sm mb-5 font-semibold">Keep track of your bookings!</span>
              <section>
                   <section>
-                       
+                       {
+                        sample.length>=1?(
+                          <section>
+                            {
+                              bookingHistory.map(()=>{
+                                return (
+                                 <div>
+
+                                </div>)
+                              })
+                            }
+                          </section>
+                        ): (
+                          <div className='flex flex-col items-center justify-center mt-[10px]'>
+                                <div><img src="/CTA.png" alt="img"  className='w-full h-full'/></div>
+                                <span className="text-[20px] font-semibold text-[#2387FE]">Your Booking History is empty at the moment</span>
+                                <button className='book-button sm:w-[200px] mt-5' onClick={()=>{ handleClick()}}>Start Booking</button>
+                          </div>
+                        )
+                       }
                   </section>
              </section>
         </section>
